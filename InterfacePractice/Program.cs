7d1e1551b17d5ea7a02c449b3f9d4f318b6d99f2
Cameron Ottley <cameron.ottley@gmail.com>
2025-10-03 15:39:25 -0600

    Built initial program

--- InterfacePractice/Program.cs
+++ InterfacePractice/Program.cs
@@ -1,2 +1,57 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Bidoof doofy = new Bidoof();
+Shuckle shuck = new Shuckle();
+
+doofy.Call();
+shuck.Call();
+
+doofy.TypeOne();
+doofy.TypeTwo();
+
+shuck.TypeOne();
+shuck.TypeTwo();
+
+interface IPokemon
+{
+    void Call();
+    void TypeOne();
+    void TypeTwo();
+}
+
+class Bidoof : IPokemon
+{
+    public void TypeOne()
+    {
+        Console.WriteLine("Type 1: Normal");
+    }
+
+    public void TypeTwo()
+    {
+        Console.WriteLine("Type 2: None");
+    }
+
+    public void Call()
+    {
+        Console.WriteLine("BOOF!!!");
+    }
+}
+
+class Shuckle : IPokemon
+{
+    public void Call()
+    {
+        Console.WriteLine("Shuck!");
+    }
+
+    public void TypeOne()
+    {
+        Console.WriteLine("Type 1: Rock");
+    }
+
+    public void TypeTwo()
+    {
+        Console.WriteLine("Type 2: Bug");
+    }
+}
